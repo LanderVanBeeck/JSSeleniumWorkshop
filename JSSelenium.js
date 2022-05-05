3ea000c7cf2ee091d52ed60038ff2e0f2e80da3d
Lander Van Beeck <landervanbeeck@hotmail.com>
2022-05-05 16:19:55 +0200

    Required fields are filled

--- JSSelenium.js
+++ JSSelenium.js
@@ -1,6 +1,9 @@
+const { By } = require('selenium-webdriver');
 var webdriver = require('selenium-webdriver');
+const { Driver } = require('selenium-webdriver/chrome');
 
 var firefox = require('selenium-webdriver/firefox');
+const by = require('selenium-webdriver/lib/by');
 
 require('geckodriver')
 
@@ -11,17 +14,17 @@ var options = new firefox.Options().setBinary('C:\\Program Files\\Mozilla Firefo
 async function test() {
 
     var driver = new webdriver.Builder().
-
     forBrowser('firefox').
-
     setFirefoxOptions(options).
-
     build();
-
     driver.get('https://demoqa.com/automation-practice-form');
 
+    driver.findElement(By.id('firstName')).sendKeys("Lander");
+    driver.findElement(By.id('lastName')).sendKeys('Van Beeck');
+    driver.findElement(By.id('userEmail')).sendKeys('landervanbeeck@hotmail.com');
+    driver.findElement(By.id('userNumber')).sendKeys('0492163907');
+    driver.findElement(By.id('gender-radio-1')).click();
+    driver.findElement(By.id('submit')).submit();
 }
 
-
-
-test()
+test();
